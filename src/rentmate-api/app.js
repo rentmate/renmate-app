345df8a082e69b505d5048aa29f6c883eb031a6c
Jimmy Pulido <jiapulidoar@gmail.com>
2020-05-06 21:07:52 -0500

    implement merge schemas and resolver

--- src/rentmate-api/app.js
+++ src/rentmate-api/app.js
@@ -1,10 +1,36 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const { ApolloServer } = require("apollo-server-express")
-const typeDefs  = require ("./src/User/typedef")
-const resolvers  = require ("./src/User/resolver")
 const app = express()
 const { getUser} = require('./src/User/services');
+
+const { mergeTypes, mergeResolvers } = require('merge-graphql-schemas')
+//import all typedefs
+const userType  = require ("./src/User/typedef")
+const chatsType  = require ("./src/Chats/typedef")
+// import all resolvers
+const userResolver  = require ("./src/User/resolver")
+const chatsResolver  = require ("./src/Chats/resolver")
+
+
+// Merge Typedefinitions
+const types = [
+  userType,
+  chatsType,
+];
+
+let typeDefs = mergeTypes(types, { all: true });
+
+
+// Merger Resolvers 
+const resolvers_concat = [
+  userResolver,
+  chatsResolver,
+];
+
+let resolvers = mergeResolvers(resolvers_concat);
+
+
 
 app.use(bodyParser.json())
 const server = new ApolloServer({
