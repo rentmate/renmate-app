46fd5e96859597d87040d73d8ccaaa8db396a8c3
marobayos <marobayos@unal.edu.co>
2020-06-24 20:01:37 -0500

    :bug: Little changes to deploy easier on ubuntu

--- src/rentmate-api/src/User/server.js
+++ src/rentmate-api/src/User/server.js
@@ -1,4 +1,4 @@
-const USER_URL = "http://rentmate-cuenta-ms:3000";
-const AUTH_URL = "http://rentmate-autenticacion-ms:3001";
+const USER_URL = "http://192.168.99.100:3000";
+const AUTH_URL = "http://192.168.99.100:3001";
 
 module.exports = {USER_URL,  AUTH_URL}
